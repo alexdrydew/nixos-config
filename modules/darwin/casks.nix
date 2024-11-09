72d339447e7bf4937effef797c24915ef0d12c08
alexdrydew <alexdrydew@toloka.ai>
2024-11-09 16:06:21 +0100

    first commit

---
+++ modules/darwin/casks.nix
@@ -0,0 +1,26 @@
+_:
+
+[
+  # Development Tools
+  "homebrew/cask/docker"
+  "dbeaver-community"
+
+  # Communication Tools
+  "discord"
+  "notion"
+  "slack"
+  "telegram"
+  "zoom"
+
+  # Utility Tools
+  "syncthing"
+
+  # Entertainment Tools
+  # "vlc"
+
+  # Productivity Tools
+  # "raycast"
+
+  # Browsers
+  "google-chrome"
+]
