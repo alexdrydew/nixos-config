cab92493708ac0127376a5e847bc4882e597ba21
alexdrydew <aleksey.suhorosov@gmail.com>
2025-01-11 19:53:10 +0100

    prune config

--- modules/nixos/packages.nix
+++ modules/nixos/packages.nix
@@ -13,33 +13,12 @@ shared-packages ++ [
   cmake
   home-manager
 
-  # Audio tools
-  cava # Terminal audio visualizer
-  pavucontrol # Pulse audio controls
-
   # Testing and development tools
   direnv
-  rofi
-  rofi-calc
   postgresql
 
-  # Screenshot and recording tools
-  flameshot
-
-  # Text and terminal utilities
-  feh # Manage wallpapers
-  screenkey
   tree
-  unixtools.ifconfig
-  unixtools.netstat
-  xorg.xwininfo # Provides a cursor to click and learn about windows
-  xorg.xrandr
 
   # File and system utilities
-  inotify-tools # inotifywait, inotifywatch - For file system events
-  i3lock-fancy-rapid
-  libnotify
-  pcmanfm # File browser
   sqlite
-  xdg-utils
 ]
