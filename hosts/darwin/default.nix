e8dbf149a15353e8a0a6393959f75f41f4eaad31
alexdrydew <aleksey.suhorosov@gmail.com>
2025-01-26 02:05:16 +0100

    move darwin system config to module

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -11,47 +11,4 @@ let user = userConfig.userName; in
   services.nix-daemon.enable = true;
   local.dock.enable = true;
 
-
-  system.checks.verifyNixPath = false;
-
-  system = {
-    stateVersion = 4;
-
-    defaults = {
-      NSGlobalDomain = {
-        AppleShowAllExtensions = true;
-        ApplePressAndHoldEnabled = false;
-
-        KeyRepeat = 2; # Values: 120, 90, 60, 30, 12, 6, 2
-        InitialKeyRepeat = 15; # Values: 120, 94, 68, 35, 25, 15
-
-        "com.apple.mouse.tapBehavior" = 1;
-        "com.apple.sound.beep.volume" = 0.0;
-        "com.apple.sound.beep.feedback" = 0;
-
-        "com.apple.keyboard.fnState" = true;
-      };
-
-      dock = {
-        autohide = false;
-        show-recents = false;
-        launchanim = true;
-        orientation = "bottom";
-        tilesize = 48;
-      };
-
-      finder = {
-        _FXShowPosixPathInTitle = false;
-      };
-
-      trackpad = {
-        Clicking = true;
-        TrackpadThreeFingerDrag = false;
-      };
-    };
-  };
-  power.sleep.display = "never";
-  services.sketchybar = {
-    enable = true;
-  };
 }
