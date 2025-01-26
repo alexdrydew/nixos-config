045afa96143dfe47e9f77dad21fb10f719724b6a
alexdrydew <aleksey.suhorosov@gmail.com>
2025-01-26 01:50:17 +0100

    move homebrew to module

--- modules/system/darwin/home-manager.nix
+++ modules/system/darwin/home-manager.nix
@@ -8,6 +8,7 @@ in
 {
   imports = [
     ./dock
+    ./homebrew
   ];
 
   users.users.${user} = {
@@ -15,26 +16,6 @@ in
     home = home;
     isHidden = false;
     shell = pkgs.zsh;
-  };
-
-  homebrew = {
-    enable = true;
-    casks = pkgs.callPackage ./casks.nix { };
-    # onActivation.cleanup = "uninstall";
-
-    # These app IDs are from using the mas CLI app
-    # mas = mac app store
-    # https://github.com/mas-cli/mas
-    #
-    # $ nix shell nixpkgs#mas
-    # $ mas search <app name>
-    #
-    # If you have previously added these apps to your Mac App Store profile (but not installed them on this system),
-    # you may receive an error message "Redownload Unavailable with This Apple ID".
-    # This message is safe to ignore. (https://github.com/dustinlyons/nixos-config/issues/83)
-    masApps = {
-      #      "1password" = 1333542190;
-    };
   };
 
   # Enable home-manager
