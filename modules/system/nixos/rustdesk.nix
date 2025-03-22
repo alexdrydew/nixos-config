6ed00825e92ed05a3ad7774047de54e01cd92b1a
alexdrydew <alexdrydew@toloka.ai>
2025-03-22 17:39:06 +0100

    docs: Add comment to clarify Rustdesk NixOS module usage

--- modules/system/nixos/rustdesk.nix
+++ modules/system/nixos/rustdesk.nix
@@ -1,4 +1,5 @@
 {...}: {
+  # ai! this is nixos module, enable this only if rustdesk-server.enable = true is defined in config
   services.rustdesk-server = {
     enable = true;
     openFirewall = true;
