6af8715a4a6eae964d1d6024eeec048fb68708ca
alexdrydew <aleksey.suhorosov@gmail.com>
2025-03-25 20:32:53 +0100

    use static ip for htpc

--- modules/system/nixos/pihole.nix
+++ modules/system/nixos/pihole.nix
@@ -1,56 +1,34 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
+{config, ...}: {
   virtualisation.containers.enable = true;
-  virtualisation.oci-containers.containers = {
-    pihole = {
-      image = "docker://pihole/pihole:latest";
-      # Expose / map ports
-      ports = ["53:53/tcp" "53:53/udp" "80:80/tcp" "443:443/tcp"];
+  virtualisation.oci-containers = {
+    backend = "podman";
+    containers = {
+      pihole = {
+        image = "docker://pihole/pihole:latest";
+        ports = [
+          "53:53/tcp"
+          "53:53/udp"
+          "80:80/tcp"
+          "443:443/tcp"
+          # DHCP
+          "67:67/udp"
+          "123:123/udp"
+        ];
 
-      # Uncomment if using Pi-hole as your DHCP server:
-      # {
-      #   hostPort      = "67";
-      #   containerPort = "67";
-      #   protocol      = "udp";
-      # }
+        environment = {
+          TZ = config.userConfig.timeZone;
+          FTLCONF_webserver_api_password = "correct horse battery staple";
+          FTLCONF_dns_listeningMode = "all";
+        };
 
-      # Uncomment if using Pi-hole as your NTP server:
-      # {
-      #   hostPort      = "123";
-      #   containerPort = "123";
-      #   protocol      = "udp";
-      # }
+        capabilities = {
+          NET_ADMIN = true;
+          SYS_TIME = true;
+          SYS_NICE = true;
+        };
 
-      # Mount local directories into the container
-      volumes = [
-        "/var/lib/pihole/etc-pihole:/etc/pihole"
-        # Uncomment if you use a custom /etc/dnsmasq.d
-        # {
-        #   source = "/var/lib/pihole/etc-dnsmasq.d";
-        #   target = "/etc/dnsmasq.d";
-        # }
-      ];
-
-      # Define environment variables
-      environment = {
-        TZ = "Europe/London";
-        FTLCONF_webserver_api_password = "correct horse battery staple";
-        FTLCONF_dns_listeningMode = "all";
+        autoStart = true;
       };
-
-      # Pass the required capabilities
-      capabilities = {
-        NET_ADMIN = true;
-        SYS_TIME = true;
-        SYS_NICE = true;
-      };
-
-      # Let systemd restart if the container stops or fails
-      # restartPolicy = "always";
-      autoStart = true;
     };
   };
 }
